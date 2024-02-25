cc31746584b4b1f964a4c42a93c67f1821e61c50
Ulfar Erlingsson <85646+UlfarErl@users.noreply.github.com>
2024-02-24 18:07:45 -0800

    background url support

--- lampgpt.py
+++ lampgpt.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
 import argparse
+from bs4 import BeautifulSoup
 import fcntl
+import hashlib
 import os
+import os.path
+import re
+import requests
 import subprocess
 import sys
 import time
@@ -11,6 +16,7 @@ import toml
 ########
 ## UTIL
 ########
+debug_prompts = False
 transcript = None
 config = None
 
@@ -32,12 +38,38 @@ def non_blocking_read(stream):
         return stream.read()
     except:
         return ""
+    
+def get_url_text_and_cache(url, regexp):
+    cachedir = "./.urlcache"
+    if not os.path.isdir(cachedir):
+        os.makedirs(cachedir)
+    cachefile = cachedir + '/' + hashlib.sha256(url.encode()).hexdigest() + '.txt'
+    if os.path.isfile(cachefile):
+        with open(cachefile, 'r') as file:
+            return file.read()
+    else:
+        webpage = requests.get(url)
+        soup = BeautifulSoup(webpage.text, 'html.parser')
+        text = soup.get_text(separator=' ', strip=True)
+        match = re.search(regexp, text)
+        if match:
+            result = match.group(1) 
+            text = regexp.replace("(.*)", result)
+            with open(cachefile,"w") as file:
+                file.write(text)
+            return text
+        else:
+            print(f"WARNING: Regexp {regexp} not matched in URL '{url}'")
+    return ""
+
 
 #############
 ## GPT STUFF
 #############
 def tell_gpt(prompt):
-    write_to_transcript(prompt)
+    global debug_prompts
+    if debug_prompts:
+        write_to_transcript(prompt)
     if not prompt[-1] == '>':
         write_to_transcript('\n')
     return
@@ -48,6 +80,12 @@ def tell_gpt(prompt):
 def init_rewrites(game_init_text):
     global config
     tell_gpt(config['init']['gpt_init'])
+    for pair in config['init']['background_urls']:
+        url = pair[0]
+        regexp = pair[1]
+        background_text = get_url_text_and_cache(url, regexp)
+        background_init = config['init']['background_init']
+        tell_gpt(background_init.replace('{{{background}}}', background_text))
     tell_gpt(config['style']['tone'])
     tell_gpt(config['style']['length'])
     tell_gpt(config['init']['startup'])
@@ -93,13 +131,16 @@ def main():
     parser.add_argument('--config', '-c', type=str, default='./lampgpt.toml', help='Configuration file name (default lampgpt.toml)')
     parser.add_argument('--transcript', '-t', type=str, help='Name of transcript output file')
     parser.add_argument('--verbose', '-v', action='store_true', help='Output original ZIL interpreter input/output')
-    parser.add_argument('--delay', '-d', type=int, default=5, help='Delay (in milliseconds) to wait for ZIL interpreter output in each turn')
+    parser.add_argument('--debug', '-d', action='store_true', help='Output all LLM inputs and outputs')
+    parser.add_argument('--delay', '-s', type=int, default=5, help='Delay (in milliseconds) to wait for ZIL interpreter output in each turn')
     parser.add_argument('--bocfel', '-x', type=str, default='./bocfel-2.1.2/bocfel', help='File name of bocfel ZIL interpreter')
     parser.add_argument('--bocfelargs', '-z', type=str, default='', help='Optional arguments for bocfel ZIL interpreter')
     parser.add_argument('ZILFILE')
     args = parser.parse_args()
 
     # Process arguments and config
+    if args.debug:
+        debug_prompts = True
     if args.transcript:
         transcript = open(args.transcript, 'w')
     try:
@@ -118,6 +159,8 @@ def main():
         time.sleep((10*args.delay)/1000.0)
         output = non_blocking_read(process.stdout)
         init_rewrites(output)
+        if args.verbose:
+            write_to_transcript(f"# ORIGINAL START: {output}")
 
         while True:
             command = sys.stdin.readline()
