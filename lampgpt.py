32f20e8e53f73e104d6b0401695d161865d0b118
Ulfar Erlingsson <85646+UlfarErl@users.noreply.github.com>
2024-02-25 16:59:41 -0800

    starcross

--- lampgpt.py
+++ lampgpt.py
@@ -15,28 +15,36 @@ import textwrap
 import time
 import toml
 
+###########
+## GLOBALS
+###########
+class GlobalState:
+    def __init__(self):
+        self.args = None        # script args
+        self.debug_log = None   # open fd for a debug log file
+        self.transcript = None  # open fd for a transcript file
+        self.config = None      # the lampgpt ToML config
+        self.game = None        # the game ToML config
+state = GlobalState()
+
 ########
 ## UTIL
 ########
-debug_log = None
-transcript = None
-config = None
-
 def write_to_debug_log(output):
-    global debug_log
-    if debug_log != None:
-        debug_log.write(output)
-        debug_log.flush()
+    global state
+    if state.debug_log != None:
+        state.debug_log.write(output)
+        state.debug_log.flush()
 
 def write_to_transcript(output, log_only=False):
-    global transcript
+    global state
     write_to_debug_log(output)
     if not log_only:
         sys.stdout.write(output)
         sys.stdout.flush()
-    if transcript:
-        transcript.write(output)
-        transcript.flush()
+    if state.transcript:
+        state.transcript.write(output)
+        state.transcript.flush()
 
 def configure_non_blocking_reads(stream):
     fd = stream.fileno()
@@ -60,8 +68,8 @@ def get_url_text_and_cache(url, regexp):
     else:
         webpage = requests.get(url)
         soup = BeautifulSoup(webpage.text, 'html.parser')
-        text = soup.get_text(separator=' ', strip=True)
-        match = re.search(regexp, text)
+        text = soup.get_text(separator='\n', strip=True)
+        match = re.search(regexp, text, re.DOTALL)
         if match:
             result = match.group(1) 
             text = regexp.replace("(.*)", result)
@@ -72,6 +80,19 @@ def get_url_text_and_cache(url, regexp):
             print(f"WARNING: Regexp {regexp} not matched in URL '{url}'")
     return ""
 
+def get_file_text(filename, regexp):
+    if os.path.isfile(filename):
+        with open(filename, 'r') as file:
+            text = file.read()
+        match = re.search(regexp, text, re.DOTALL)
+        if match:
+            result = match.group(1) 
+            return regexp.replace("(.*)", result)
+        else:
+            print(f"WARNING: Regexp {regexp} not matched in file '{filename}'")
+    else:
+        print(f"WARNING: File {filename} not found as background info.")
+    return ""
 
 #############
 ## GPT STUFF
@@ -82,7 +103,7 @@ gpt_message = ""
 gpt_model = None
 gpt_temp = None
 def add_to_gpt_prompt(prompt):
-    global debug_log, gpt_message
+    global gpt_message
     write_to_debug_log(prompt)
     if not prompt[-1] == '>':
         gpt_message = gpt_message + '\n'
@@ -106,18 +127,29 @@ def get_gpt_response(message_type):
 ## REWRITE LOGIC
 #################
 def init_rewrites(game_init_text, style):
-    global config
-    add_to_gpt_prompt(config['init']['gpt_init'])
-    for pair in config['init']['background_urls']:
+    global state
+    add_to_gpt_prompt(state.config['init']['gpt_init'])
+
+    background_init = state.config['init']['background_init']
+    add_to_gpt_prompt(background_init.replace('{{{background}}}', state.game['game']['setup']))
+    for pair in state.game['game']['background_urls']:
         url = pair[0]
         regexp = pair[1]
-        background_text = get_url_text_and_cache(url, regexp)
-        background_init = config['init']['background_init']
-        add_to_gpt_prompt(background_init.replace('{{{background}}}', background_text))
-    add_to_gpt_prompt(config['style']['tone_'+style])
-    add_to_gpt_prompt(config['style']['length'])
-    add_to_gpt_prompt(config['init']['startup'])
-    add_to_gpt_prompt(config['style']['formatting'])
+        background_text = get_url_text_and_cache(url, regexp).strip()
+        if background_text != '':
+            add_to_gpt_prompt(background_init.replace('{{{background}}}', background_text))
+    for pair in state.game['game']['background_files']:
+        filename = pair[0]
+        regexp = pair[1]
+        background_text = get_file_text(filename, regexp).strip()
+        if background_text != '':
+            add_to_gpt_prompt(background_init.replace('{{{background}}}', background_text))
+
+    add_to_gpt_prompt(state.config['style']['tone_'+style])
+    add_to_gpt_prompt(state.config['style']['length'])
+    add_to_gpt_prompt(state.config['init']['startup'])
+    add_to_gpt_prompt(state.config['style']['formatting'])
+
     if game_init_text[-1] == '>': # remove input prompt, if there
         game_init_text = game_init_text[:-1] 
         game_init_text = game_init_text.rstrip()
@@ -125,7 +157,7 @@ def init_rewrites(game_init_text, style):
     return get_gpt_response('system')
 
 def rewrite_response(command, response, style):
-    global config
+    global state
     # clean up input/output
     input = False
     if response[-1] == '>': # remove input prompt, if there
@@ -136,22 +168,22 @@ def rewrite_response(command, response, style):
 
     # detect errors
     error = False
-    for prefix in config['errors']['prefixes']:
+    for prefix in state.config['errors']['prefixes']:
         if response.startswith(prefix):
             error = True
     
     # rewrite
-    template = config['responses']['generic']
+    template = state.config['responses']['generic']
     prompt = template.replace('{{{command}}}',command).replace('{{{response}}}',response)
     add_to_gpt_prompt(prompt)
     if error:
-        add_to_gpt_prompt(config['errors']['generic'])
+        add_to_gpt_prompt(state.config['errors']['generic'])
     else:
-        add_to_gpt_prompt(config['style']['tone_'+style])
-        add_to_gpt_prompt(config['style']['length'])
-        add_to_gpt_prompt(config['style']['caveat'])
-        add_to_gpt_prompt(config['style']['formatting'])
-        add_to_gpt_prompt(config['responses']['suffix'])
+        add_to_gpt_prompt(state.config['style']['tone_'+style])
+        add_to_gpt_prompt(state.config['style']['length'])
+        add_to_gpt_prompt(state.config['style']['caveat'])
+        add_to_gpt_prompt(state.config['style']['formatting'])
+        add_to_gpt_prompt(state.config['responses']['suffix'])
     gpt_response = get_gpt_response('user')
     
     write_to_transcript(textwrap.fill(gpt_response, width=80))
@@ -163,7 +195,7 @@ def rewrite_response(command, response, style):
 ## MAIN
 ########
 def main(): 
-    global transcript, config, debug_log, gpt_client, gpt_model, gpt_temp
+    global state, gpt_client, gpt_model, gpt_temp
     parser = argparse.ArgumentParser(description='Script for an LLM-enhanced Infocom experience.')
     
     # Parse arguments
@@ -177,20 +209,29 @@ def main():
     parser.add_argument('--gpt_temp', '-T', type=int, default=5, help='Temperature of LLM model (integer percent; default 5%)')
     parser.add_argument('--gpt_auth', '-A', type=str, default='****MISSING API KEY****', help='Authorization token for LLM')
     parser.add_argument('--llm', '-L', type=str, default='gpt-4-turbo-preview', help='LLM to use (default gpt-4)')
-    parser.add_argument('--style', '-S', type=str, default='pratchett', help='Style: pratchett, gumshoe, ...')
-    parser.add_argument('ZILFILE')
+    parser.add_argument('--style', '-S', type=str, default='spaceopera', help='Style: pratchett, gumshoe, ...')
+    parser.add_argument('GAMENAME')
     args = parser.parse_args()
 
     # Process arguments and config
+    state.args = args
     if args.debug:
-        debug_log = open('./debug.log', 'w')
+        state.debug_log = open('./debug.log', 'w')
     if args.transcript:
-        transcript = open(args.transcript, 'w')
+        state.transcript = open(args.transcript, 'w')
     try:
         with open(args.config, 'r') as config_file:
             config = toml.load(config_file)
+            state.config = config
     except:
         print('ERROR: Invalid or missing ToML configuration file (default is ./lampgpt.toml)')
+        return
+    try:
+        with open(args.GAMENAME + '.toml', 'r') as game_file:
+            game = toml.load(game_file)
+            state.game = game
+    except:
+        print(f'ERROR: Invalid or missing {args.GAMENAME} ToML file')
         return
     if config['style'].get('tone_'+args.style) == None:
         print(f'ERROR: Unknown LLM rewriting style "{args.style}"')
@@ -202,7 +243,7 @@ def main():
     gpt_client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY", args.gpt_auth))
 
     # Launch the ZIL interpreter and manage its input/output
-    bocfel_launch = [args.bocfel] + args.bocfelargs.split() + [args.ZILFILE]
+    bocfel_launch = [args.bocfel] + args.bocfelargs.split() + [game['game']['path']]
     process = subprocess.Popen(bocfel_launch, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, bufsize=1)
     configure_non_blocking_reads(process.stdout)
 
@@ -210,7 +251,7 @@ def main():
         time.sleep((10*args.delay)/1000.0)
         output = non_blocking_read(process.stdout)
         start = init_rewrites(output, args.style)
-        if debug_log != None and args.verbose:
+        if state.debug_log != None and args.verbose:
             write_to_transcript(f"# ORIGINAL START: {output}")
         write_to_transcript(textwrap.fill(start, width=80))
         write_to_transcript('\n\n>')
@@ -224,11 +265,11 @@ def main():
             # get the original ZIL interpreter response
             process.stdin.write(command)
             process.stdin.flush()
-            if debug_log != None and args.verbose:
+            if state.debug_log != None and args.verbose:
                 write_to_transcript(f"# ORIGINAL COMMAND: {command}")
             time.sleep((1.0*args.delay)/1000.0)
             response = non_blocking_read(process.stdout)
-            if debug_log != None and args.verbose:
+            if state.debug_log != None and args.verbose:
                 write_to_transcript(f"# ORIGINAL RESPONSE: {response}")
 
             # get the GPT version of the command and the GPT-modified response
